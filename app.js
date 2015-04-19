f04dfe266e83b7234b967a0f70165b1e2fc37df4
cmmisura14 <cmmisura14@gmail.com>
2015-04-19 18:46:22 -0500

    Commit Numba 7

--- app.js
+++ app.js
@@ -1,93 +1,85 @@
  function Monster(name, strength, dexterity, constitution, wisdom, intelligence, charisma){
-	this.name = name;
-	this.strength = strength;
-	this.dexterity = dexterity;
-	this.constitution = constitution;
-	this.wisdom = wisdom;
-	this.intelligence = intelligence;
-	this.charisma = charisma;
-}
+ 	this.name = name;
+ 	this.strength = strength;
+ 	this.dexterity = dexterity;
+ 	this.constitution = constitution;
+ 	this.wisdom = wisdom;
+ 	this.intelligence = intelligence;
+ 	this.charisma = charisma;
+ }
 
-//(Math.floor(Math.random() * 20) + 1)
-
-Monster.prototype = {
-	attack : function(){
-		return this.strength;
-	}
-}
+Monster.prototype = {attack : function(){return this.strength * (Math.floor(Math.random() * 20) + 1);}}
 
 Player.prototype = new Monster;
 
-
-Player.prototype.attack = function(){
-	return this.dexterity;
-}
+Player.prototype.attack = function(){return this.dexterity * (Math.floor(Math.random() * 20) + 1);}
 
 function Player(name, strength, dexterity, constitution, wisdom, intelligence, charisma, playerClass){
 	this.playerClass = playerClass;
 	Monster.apply(this, arguments);
 }
 
-
-var monsterMary = new Monster("Mary", 10, 5, 7, 4, 9, 4);
-var playerCody = new Player("Cody", 15, 4, 9, 6, 3, 8, "goblin");
-
-
-var dungeonArray = [];
+var latestAttack = document.getElementById("lastAttacker");
+var amazingAttack = document.getElementById("amazingAttack");
 
 function displayCreature(object){
 	var table = document.getElementById('statsTable');
 	var row = table.insertRow(table.rows.length);
-	var name = row.insertCell(0);
-	var strength = row.insertCell(1);
-	var dexterity = row.insertCell(2);
-	var constitution = row.insertCell(3);
-	var wisdom = row.insertCell(4);
-	var intelligence = row.insertCell(5);
-	var charisma = row.insertCell(6);
-	var playerClass = row.insertCell(7);
-	name.innerHTML = makeButton(object);
-    strength.innerHTML = object.strength;
-	dexterity.innerHTML = object.dexterity;
-	constitution.innerHTML = object.constitution;
-	wisdom.innerHTML = object.wisdom;
-	intelligence.innerHTML = object.intelligence;
-	charisma.innerHTML = object.charisma;
-	playerClass.innerHTML = object.playerClass;
-
-}
-
-function makeButton(object) {
-	console.log(object.name);
-	return "<input type='button' id='attackLog' onclick ='displayAttack()' value='" + object.name + "'/>"
-}
-
-function currentAttack(name){
-	for(var i = 0; i < dungeonArray.length; i++){
-		if(dungeonArray[i].name == name){
-			return dungeonArray[i].attack();
-		}
+	row.insertCell(0).appendChild(makeButton(object));
+	var requiredAttributes = [object.strength, object.dexterity, object.constitution, object.wisdom, object
+	.intelligence, object.charisma];
+	for (var i = 0; i < requiredAttributes.length; i++) {
+		row.insertCell(i+1).innerHTML = requiredAttributes[i];
+	} 
+	if (object.playerClass){ 
+		row.insertCell(7).innerHTML = object.playerClass;
+	} else {
+		row.insertCell(7).innerHTML = '';
 	}
 }
 
-function displayAttack() {
-	console.log(document.getElementById('attackLog').getAttribute('value'));
+function makeButton(object) {
+	var newButton = document.createElement('input');
+	newButton.type = 'button';
+	newButton.id = 'currentCreatures'
+	newButton.value = object.name;
+	newButton.onclick = function(){ 
+		lastAttacker.innerHTML = object.name + ' just ATTACKED'; 
+		amazingAttack.innerHTML = (object.playerClass ? 'with a Dexterity of ' : ' with a Strength of ') + object.attack(object.name);
+	};
+	return newButton;
 }
 
-function populateArray(){
+function clearForm() {
+	document.getElementById('inputForm').reset();
+}
+
+function addEntry(){
+	if(checkForm()){
+		newEntry();
+		clearForm();
+	} else {
+		alert("Please fill out all required fields!");
+	}
+		
+}
+
+function newEntry(){
 	var newCreature;
-	if(document.getElementById('playerClass').value){
-			newCreature = new Player(document.getElementById('name').value, document.getElementById('strength').value, document.getElementById('dexterity').value, document.getElementById('constitution').value, document.getElementById('wisdom').value, document.getElementById('intelligence').value, document.getElementById('charisma').value, document.getElementById('playerClass').value); 
-			
-		} else {
-			newCreature = new Monster(document.getElementById('name').value, document.getElementById('strength').value, document.getElementById('dexterity').value, document.getElementById('constitution').value, document.getElementById('wisdom').value, document.getElementById('intelligence').value, document.getElementById('charisma').value);
-		}
+	if(document.querySelector('input[name = "class"]:checked')) {
+			newCreature = new Player(document.getElementById('name').value.toUpperCase(), document.getElementById('strength').value, document.getElementById('dexterity').value, document.getElementById('constitution').value, document.getElementById('wisdom').value, document.getElementById('intelligence').value, document.getElementById('charisma').value, document.querySelector('input[name = "class"]:checked').value.toUpperCase()); 
 
-		dungeonArray.push(newCreature);
+		} else {
+			newCreature = new Monster(document.getElementById('name').value.toUpperCase(), document.getElementById('strength').value, document.getElementById('dexterity').value, document.getElementById('constitution').value, document.getElementById('wisdom').value, document.getElementById('intelligence').value, document.getElementById('charisma').value);
+		}
 		displayCreature(newCreature);
-		displayAttack(newCreature)
 }
 
-document.getElementById('addMonster').addEventListener('click', populateArray);
+function checkForm(){
+	return document.getElementById('name').value && document.getElementById('strength').value && document.getElementById('dexterity').value && document.getElementById('constitution').value && document.getElementById('wisdom').value && document.getElementById('intelligence').value && document.getElementById('charisma').value ? true : false
 
-//document.getElementById('attackLog').addEventListener('click', console.log("hi"));
+}
+
+document.getElementById('addMonster').addEventListener('click', addEntry);
+
+
